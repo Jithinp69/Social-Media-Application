f4accb3cc294fd18b83d22eaf16f497fcf354220
vineetkrdixit <vineetdixit.vns@gmail.com>
2022-05-22 23:38:29 +0530

    WIP :Done home Page

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,22 +5,33 @@ import "../Components/Header.css";
 
 export default function Header() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
           <img className="logo" src={logo} alt="logo" />
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+        <div className="">
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav navbarsections">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Profile
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"></a>
+              </li>
+            </ul>
+            <div className="welcome">
+              <h6>Welcome {} </h6>
+            </div>
+          </div>
+        </div>
       </div>
     </nav>
   );
